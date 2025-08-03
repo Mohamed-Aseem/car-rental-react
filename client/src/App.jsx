8eea9a7bcd719a3f0d5a28a04eabe1f5ede915da
Mohamed-Aseem <ima7str@gmail.com>
2025-08-03 22:30:32 +0530

    Create Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,31 +11,35 @@ import Dashboard from './pages/owner/Dashboard'
 import AddCar from './pages/owner/AddCar'
 import ManageCars from './pages/owner/ManageCars'
 import ManageBookings from './pages/owner/ManageBookings'
+import Login from './components/Login'
 
 const App = () => {
-  const [showLogin ,setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
   const isOwnerPath = useLocation().pathname.startsWith('/owner')
 
   return (
     <>
-      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
-      
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+      {
+        showLogin && <Login setShowLogin={setShowLogin} />
+      }
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/car-details/:id' element={<CarDetails />}/>
-        <Route path='/cars' element={<Cars />}/>
-        <Route path='/my-bookings' element={<MyBookings />}/>
-        
+        <Route path='/' element={<Home />} />
+        <Route path='/car-details/:id' element={<CarDetails />} />
+        <Route path='/cars' element={<Cars />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+
         <Route path='/owner' element={<Layout />}>
-          <Route index element={<Dashboard />}/>
-          <Route path='add-car' element={<AddCar />}/>
-          <Route path='manage-cars' element={<ManageCars />}/>
-          <Route path='manage-bookings' element={<ManageBookings />}/>
+          <Route index element={<Dashboard />} />
+          <Route path='add-car' element={<AddCar />} />
+          <Route path='manage-cars' element={<ManageCars />} />
+          <Route path='manage-bookings' element={<ManageBookings />} />
         </Route>
 
       </Routes>
       {!isOwnerPath && <Footer />}
-      
+
     </>
   )
 }
