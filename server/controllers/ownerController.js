6987565143f3e9fa8adc392d952a7b6ab855155c
Mohamed-Aseem <ima7str@gmail.com>
2025-08-18 22:31:02 +0530

    Add imagekit

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,19 +1,67 @@
 import User from '../models/User.js'
+import Car from '../models/Car.js'
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js'
 
-export const changeRoleToOwner = async (req, res) =>{
+export const changeRoleToOwner = async (req, res) => {
     try {
         const { _id } = req.user;
-        await User.findByIdAndUpdate(_id, {role:"owner"})
+        await User.findByIdAndUpdate(_id, { role: "owner" })
         res.json({
-            success:true,
-            message:"Now you can list cars"
+            success: true,
+            message: "Now you can list cars"
         })
     } catch (error) {
         console.log(error.message)
         res.json({
-            success:false,
+            success: false,
             message: error.message
         })
     }
 
+}
+
+//API to List Car
+export const addCar = async (req, res) => {
+    try {
+        const { _id } = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+
+        //upload image to imagekit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        })
+
+        // Optimization through imagekit URL transformation
+        var optimizedImageURL = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {width:'1280'},
+                {quality:'auto'}, //Auto compression
+                {format:'webp'}
+            ]
+        });
+
+        const image = optimizedImageURL;
+
+        await Car.create({...car, owner:_id, image})
+
+        res.json({
+            success:true,
+            message:"Car Added"
+        })
+
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
